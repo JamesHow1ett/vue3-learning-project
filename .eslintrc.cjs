24d1fecfff5dcc1e75a7e223384bc17fc31a564c
JamesHow1ett <mopis101@gmail.com>
2023-02-26 15:48:00 +0200

    chore: Add airbnb eslint config

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -4,14 +4,20 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
   root: true,
   extends: [
+    'plugin:vue/base',
     'plugin:vue/vue3-essential',
+    'plugin:vue/vue3-strongly-recommended',
+    'plugin:vue/vue3-recommended',
     'eslint:recommended',
+    'airbnb-base',
     '@vue/eslint-config-prettier/skip-formatting',
   ],
   parserOptions: {
     ecmaVersion: 'latest',
   },
   rules: {
+    'import/prefer-default-export': 'off',
+    'import/no-extraneous-dependencies': 'off',
     'no-unused-vars': 'warn',
   },
 };
