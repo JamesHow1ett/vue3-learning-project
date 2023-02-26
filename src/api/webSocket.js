2fc338c2e042a02f9784d20a2f7e24a463f010af
JamesHow1ett <mopis101@gmail.com>
2023-02-26 13:35:54 +0200

    feature: Add api/webSocket.js to use in future cases

---
+++ src/api/webSocket.js
@@ -0,0 +1,18 @@
+import { BASE_URL_SOCKET, API_KEY } from './constants';
+
+const url = new URL(BASE_URL_SOCKET);
+url.searchParams.append('api_key', API_KEY);
+
+const ccStreamer = new WebSocket(url);
+
+ccStreamer.onopen = function onStreamOpen() {
+  const subRequest = {
+    action: 'SubAdd',
+    subs: ['2~Coinbase~BTC~USD'],
+  };
+  ccStreamer.send(JSON.stringify(subRequest));
+};
+
+ccStreamer.onmessage = function onStreamMessage(event) {
+  console.log('Received from Cryptocompare: ' + event);
+};
