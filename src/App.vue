e681ced144ee223e9602163d1c00fe8bfe18632c
JamesHow1ett <mopis101@gmail.com>
2023-03-01 23:15:34 +0200

    feature: add filter and pagination

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <script setup>
-import { onMounted, ref, watch } from 'vue';
+import { onMounted, ref, watch, computed } from 'vue';
 import { storeToRefs } from 'pinia';
 import { useTickerStore } from './stores/tickers';
 import { TIMER_DELAY } from './utils/constants';
@@ -14,8 +14,13 @@ const { THREE_SECONDS, FIVE_SECONDS } = TIMER_DELAY;
 let intervalUpdateGraph;
 let intervalUpdateTickersData;
 const tickerInput = ref('');
+const filterInput = ref('');
 const errorIsTickerAdded = ref(false);
 const typeSuggestions = ref(defaultTypeSuggestions);
+const pagination = ref({
+  startIdx: 0,
+  endIdx: 6,
+});
 
 const store = useTickerStore();
 const { allCoins, tickers, loading, currentTicker } = storeToRefs(store);
@@ -23,6 +28,30 @@ const { allCoins, tickers, loading, currentTicker } = storeToRefs(store);
 onMounted(async () => {
   await store.fetchAllCoinsList();
 });
+
+const filteredTickers = computed(() =>
+  tickers.value.filter((ticker) => ticker.name.includes(filterInput.value.toUpperCase()))
+);
+
+const paginatedTickers = computed(() =>
+  filteredTickers.value.slice(pagination.value.startIdx, pagination.value.endIdx)
+);
+
+const currentPage = computed(() => pagination.value.endIdx / 6);
+
+const allPages = computed(() => {
+  const hasRemainderOfDivision = filteredTickers.value.length % 6 > 0;
+
+  if (hasRemainderOfDivision) {
+    return Math.floor(filteredTickers.value.length / 6) + 1;
+  }
+
+  return Math.floor(filteredTickers.value.length / 6);
+});
+
+const hasPrevPage = computed(() => currentPage.value > 1);
+
+const hasNextPage = computed(() => filteredTickers.value.length > pagination.value.endIdx);
 
 watch(tickers, (newValue, oldValue) => {
   if (!newValue.length) {
@@ -53,26 +82,27 @@ watch(currentTicker, (value) => {
 watch(
   () => tickerInput.value,
   (tickerName) => {
-    const addedTicker = store.getTickerByName(tickerName.toUpperCase());
+    const tickerNameUpperCase = tickerName.toUpperCase();
+    const addedTicker = store.getTickerByName(tickerNameUpperCase);
     errorIsTickerAdded.value = !!addedTicker;
 
     const allSuggestions = [];
 
     Object.values(allCoins.value).forEach((coin) => {
-      if (coin.Symbol === tickerName.toUpperCase()) {
+      if (coin.Symbol === tickerNameUpperCase) {
         allSuggestions.push(coin);
         return;
       }
 
       const finded =
-        coin.Symbol.includes(tickerName.toUpperCase()) || coin.FullName.includes(tickerName);
+        coin.Symbol.includes(tickerNameUpperCase) || coin.FullName.includes(tickerName);
 
       if (finded) {
         allSuggestions.push(coin);
       }
     });
 
-    const exactMatch = allSuggestions.find((coin) => coin.Symbol === tickerName.toUpperCase());
+    const exactMatch = allSuggestions.find((coin) => coin.Symbol === tickerNameUpperCase);
     let suggestionList = [];
 
     if (exactMatch) {
@@ -120,6 +150,20 @@ function selectTicker(tickerName) {
   }
 
   store.selectTicker(tickerName);
+}
+
+function prevPage() {
+  if (hasPrevPage.value) {
+    pagination.value.startIdx -= 6;
+    pagination.value.endIdx -= 6;
+  }
+}
+
+function nextPage() {
+  if (hasNextPage.value) {
+    pagination.value.startIdx += 6;
+    pagination.value.endIdx += 6;
+  }
 }
 </script>
 
@@ -190,9 +234,42 @@ function selectTicker(tickerName) {
 
       <template v-if="tickers.length">
         <hr class="w-full border-t border-gray-600 my-4" />
+        <div class="max-w-xs">
+          <label for="filter" class="block text-sm font-medium text-gray-700">Filter</label>
+          <div class="mt-1 relative rounded-md shadow-md">
+            <input
+              id="filter"
+              v-model="filterInput"
+              type="text"
+              name="filter"
+              class="block w-full pr-10 pl-2 py-2 border-gray-300 text-gray-900 focus:outline-none focus:ring-gray-500 focus:border-gray-500 sm:text-sm rounded-md"
+              placeholder="Example DOGE"
+            />
+          </div>
+          <div class="mt-4">
+            <button
+              :disabled="!hasPrevPage"
+              type="button"
+              class="inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-25"
+              @click="prevPage"
+            >
+              Prev
+            </button>
+            <span class="mx-4 font-medium">Page: {{ currentPage }}/{{ allPages }}</span>
+            <button
+              :disabled="!hasNextPage"
+              type="button"
+              class="inline-flex items-center py-2 px-4 border border-transparent shadow-sm text-sm leading-4 font-medium rounded-full text-white bg-gray-600 hover:bg-gray-700 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500 disabled:opacity-25"
+              @click="nextPage"
+            >
+              Next
+            </button>
+          </div>
+        </div>
+        <hr class="w-full border-t border-gray-600 my-4" />
         <dl class="mt-5 grid grid-cols-1 gap-5 sm:grid-cols-3">
           <ticker-item
-            v-for="ticker in tickers"
+            v-for="ticker in paginatedTickers"
             :key="ticker.name"
             :ticker="ticker"
             @remove-ticker="removeTicker"
